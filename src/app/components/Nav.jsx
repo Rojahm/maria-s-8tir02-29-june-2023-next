1fab5d40be2693786cad14256aa42a29a8b5e5b6
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-08-05 21:09:40 +0330

    navbar

---
+++ src/app/components/Nav.jsx
@@ -0,0 +1,15 @@
+const Nav = () => {
+  return (
+    <div>
+      <div className="navbar">
+        <ul>
+          <li className="nav-item">SHOP</li>
+          <li className="nav-item">ABOUT</li>
+          <li className="nav-item">CONTACT</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
