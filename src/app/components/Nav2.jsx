945edb2c879871aea4cc6582001f84f5c769684b
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-08-05 21:09:31 +0330

    LOGIN nav

---
+++ src/app/components/Nav2.jsx
@@ -0,0 +1,15 @@
+const Nav2 = () => {
+  return (
+    <div>
+      <div className="navbar">
+        <ul>
+          <li className="nav-item">searchIcon</li>
+          <li className="nav-item">LOG IN</li>
+          <li className="nav-item">CART (0)</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Nav2;
