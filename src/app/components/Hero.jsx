bbd7dc85d59a89dcd324437da72a3f1fcb0bb016
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-08-07 20:18:26 +0330

    added Hero component to the Homepage

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,7 +1,30 @@
+import Image from "next/image";
+
 const Hero = () => {
   return (
-    <div>
-      <h1>HERO</h1>
+    <div className="Hero">
+      <div>
+        <span style={{ color: "#029381", fontSize: 50 }}>&#10022;</span>
+        <h4 className="title-secondary">BE ALWAYS YOUNG</h4>
+        <h1 className="title-primary">Discover the gourgeous shine with us</h1>
+        <p className="hero-paragraph">
+          Each and every formula contains ingredients in the dosage that will do
+          the most for your skin right now.
+        </p>
+        <button className="hero-btn">View Products</button>
+        <div style={{ display: "flex", justifyContent: "flex-end" }}>
+          <span style={{ color: "#F0C396", fontSize: 120 }}>&#10022;</span>
+        </div>
+      </div>
+      <div className="hero-right">
+        <Image
+          src="/img/Hero/img1.JPG"
+          width={400}
+          height={550}
+          alt="eye behinf a big leaf"
+        />
+        <span style={{ color: "#C65656", fontSize: 50 }}>&#10022;</span>
+      </div>
     </div>
   );
 };
