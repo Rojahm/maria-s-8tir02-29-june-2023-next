679dfbe6a7d27ed161374a13fd76f05af17855b3
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-08-07 17:02:53 +0330

    Header component

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Nav from "./Nav";
 import Nav2 from "./Nav2";
 import Image from "next/image";
@@ -6,17 +7,18 @@ const Header = () => {
   return (
     <div className="Header">
       <Nav />
-      <div>
-        <div className="logo">
+
+      <div className="logo">
+        <Link href="/">
           <Image
             src="/img/hq-logo-color.png"
             width={120}
             height={120}
             alt="color logo maria cosmetics"
           />
-          <span className="title">mariaCosmetics</span>
-        </div>
+        </Link>
       </div>
+
       <Nav2 />
     </div>
   );
