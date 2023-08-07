94844e0ee8804fedc98a795b4d385e9b312bcfc9
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-08-07 20:18:50 +0330

    added hr before and after Hero component

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,9 @@ import Hero from "./components/Hero";
 export default function Home() {
   return (
     <main>
+      <hr />
       <Hero />
+      <hr />
       <h1> HomePage</h1>
     </main>
   );
