d1295329e321a27be7527136d624aa19c3f181f1
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-08-08 20:36:06 +0330

    remove the hr tags from page and added it to the layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,20 @@
+// add bootstrap css
+import "bootstrap/dist/css/bootstrap.css";
+import Head from "next/head";
 import Image from "next/image";
 import styles from "./page.module.css";
 import Hero from "./components/Hero";
 
 export default function Home() {
   return (
-    <main>
-      <hr />
-      <Hero />
-      <hr />
-      <h1> HomePage</h1>
-    </main>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main>
+        <Hero />
+        <h1> HomePage</h1>
+      </main>
+    </>
   );
 }
