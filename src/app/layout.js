b2880dcb91af5b3dca9dace66eb60b212e20cb20
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-08-08 20:35:32 +0330

    added an hr tag after header

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,6 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Header />
+        <hr />
         <div className="container">{children}</div>
         <Footer />
       </body>
