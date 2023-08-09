631a4f44f78cc0bb47fe2bd6e11b78b4a2c6cee6
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-08-09 23:23:12 +0330

    filled Footer component and style

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,7 +1,68 @@
+import Link from "next/link";
+import Image from "next/image";
+
 function Footer() {
   return (
-    <div className="Footer">
-      <h2>Footer</h2>
+    <div className="Footer container shadow-lg">
+      <div className="row justify-content-evenly mb-5">
+        <div className="col col-3">
+          <Image
+            src={"/img/logo-color-high-thick.png"}
+            width={200}
+            height={200}
+            layout="responsive"
+            alt="color logo of maria cosmetics"
+          />
+        </div>
+        <div className="col col-8 info">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum
+          </p>
+        </div>
+      </div>
+      <div className="d-flex">
+        <div className="col footer-col">
+          <h4 className="footer-title">title1</h4>
+          <p> address and info</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum
+          </p>
+        </div>
+        <div className="vr"></div>
+        <div className="col footer-col">
+          <h4 className="footer-title">title2</h4>
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+          </ul>
+        </div>
+        <div className="vr"></div>
+        <div className="col footer-col">
+          <h4 className="footer-title">title3</h4>
+          <p className="footet-content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
