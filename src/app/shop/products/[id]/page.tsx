a6e1a7bf11e68b9238ebf784e1d70c45e83e54c1
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-08-11 18:08:14 +0330

    converted jsx to tsx

--- src/app/shop/products/[id]/page.tsx
+++ src/app/shop/products/[id]/page.tsx
@@ -3,19 +3,9 @@ import LoadingPage from "@/app/loading";
 import Product from "@/app/components/Product";
 import ProductDetail from "@/app/components/ProductDetail";
 
-const ProductPage = ({ params: { id } }) => {
+const ProductPage = ({ params: { id } }: any) => {
   return (
     <div className="ProductPage container">
-      <nav style={{ ariaLabel: "breadcrumb" }}>
-        <ol class="breadcrumb">
-          <li class="breadcrumb-item">
-            <a href="#">Home</a>
-          </li>
-          <li class="breadcrumb-item active" aria-current="page">
-            Library
-          </li>
-        </ol>
-      </nav>
       <div className="card mb-3 shadow" style={{ maxWidth: "90vw" }}>
         <div className="row g-0">
           <div className="col-md-4 product">
