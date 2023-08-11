d89c503583d35b790598b0b61265957624330183
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-08-11 18:07:31 +0330

    converted to tsx- addto cart component added with its props

--- src/app/components/ProductDetail.tsx
+++ src/app/components/ProductDetail.tsx
@@ -1,18 +1,30 @@
-async function fetchProduct(id) {
+import AddToCart from "./AddToCart";
+import type { ProductProps } from "./Product";
+
+async function fetchProduct(id: number) {
   const response = await fetch(
     `https://makeup-api.herokuapp.com/api/v1/products/${id}.json`
   );
   const product = await response.json();
   return product;
 }
-const ProductDetail = async ({ id }) => {
+
+interface ColorObj {
+  color_name: string;
+  hex_value: string;
+}
+export interface product {
+  product: ProductProps;
+}
+
+const ProductDetail = async ({ id }: ProductProps) => {
   const product = await fetchProduct(id);
   return (
     <div>
       <h2>{product.name}</h2>
       <p>{product.description}</p>
       <div>
-        {product.product_colors.map((color) => (
+        {product.product_colors.map((color: ColorObj) => (
           <button
             key={color.color_name}
             style={{
@@ -29,12 +41,14 @@ const ProductDetail = async ({ id }) => {
       </div>
       <div>
         <span>{product.price}$</span>
-        <span>
-          <button className="btn btn-dark">+</button>
-          <button className="btn">0</button>
-          <button className="btn btn-dark">-</button>
-        </span>
       </div>
+
+      <AddToCart
+        name={product.name}
+        id={product.id}
+        price={product.price}
+        quantity={product.quantity}
+      />
     </div>
   );
 };
