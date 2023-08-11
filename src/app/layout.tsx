f9b6d3ceb3de1222dd42f245de08eaa08c03cdf4
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-08-11 20:40:13 +0330

    import Cart component into layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { Inter } from "next/font/google";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Credit from "./components/Credit";
+import Cart from "@/app/components/Cart/Cart";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,6 +27,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <Providers>
           <Header />
           <hr />
+          <Cart />
           <div className="container">{children}</div>
           <Footer />
           <Credit />
