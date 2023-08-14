653ea475c4f936ea6068571a90b65f3f4eec716e
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-08-14 15:44:58 +0330

    product detail type what i guessed!

--- src/redux/reducers/cart/cartSlice.tsx
+++ src/redux/reducers/cart/cartSlice.tsx
@@ -1,6 +1,32 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import type { RootState } from "@/redux/store";
 
+export interface ProductDetail {
+  id: number;
+  brand: string;
+  name: string;
+  price: number;
+  price_sign: string;
+  currency: string;
+  image_link: URL;
+  product_link: URL;
+  website_link: URL;
+  description: string;
+  rating: number;
+  category: string;
+  product_type: string;
+  tag_list: [];
+  created_at: Date;
+  updated_at: Date;
+  product_api_url: URL;
+  api_featured_image: URL;
+  product_colors: [
+    {
+      hex_value: `#${string}`;
+      colour_name: string;
+    }
+  ];
+}
 interface Product {
   name: string;
   id: number;
