507d3ff22dc82ef29c8dce2e962468a6022125a8
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-08-18 20:24:33 +0330

    cart btn updated

--- src/app/components/Nav2.tsx
+++ src/app/components/Nav2.tsx
@@ -1,13 +1,7 @@
-"use client";
 import Link from "next/link";
-import { useAppSelector } from "@/redux/hooks";
-import { FaShoppingCart } from "react-icons/fa";
-import { useAppDispatch } from "@/redux/hooks";
-import { showCart } from "@/redux/reducers/cart/cartSlice";
+import CartBtn from "./Cart/CartBtn";
 
 const Nav2 = () => {
-  const count = useAppSelector((state) => state.cart.totalQuantity);
-  const dispatch = useAppDispatch();
   return (
     <div className="navbar">
       <Link href="/search" className="nav-item">
@@ -16,17 +10,8 @@ const Nav2 = () => {
       <Link href="/login" className="nav-item">
         LOGIN
       </Link>
-      <button
-        className="nav-item position-relative btn"
-        id="liveToastBtn"
-        onClick={() => dispatch(showCart())}
-      >
-        <FaShoppingCart style={{ fontSize: "30px" }} />
-        <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-success">
-          {count}
-          <span className="visually-hidden">Cart Items</span>
-        </span>
-      </button>
+
+      <CartBtn />
     </div>
   );
 };
