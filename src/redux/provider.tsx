1359b715c177724d42d5d923964f18343e26fbc6
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-08-18 20:27:56 +0330

    interface Props updated

--- src/redux/provider.tsx
+++ src/redux/provider.tsx
@@ -1,11 +1,11 @@
 "use client";
-
+import React from "react";
 import { Provider } from "react-redux";
 import { store } from "./store";
 
-export interface ProvidersProps {
+interface Props {
   children: React.ReactNode;
 }
-export function Providers({ children }: ProvidersProps) {
+export function Providers({ children }: Props) {
   return <Provider store={store}>{children}</Provider>;
 }
