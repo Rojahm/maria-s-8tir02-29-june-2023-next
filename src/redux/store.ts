d6eaf5daeadc92b3dc61a4d111f618e8efccf5bd
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-08-18 20:27:30 +0330

    store updated for cart

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,8 @@
-"use client";
-
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./reducers/counter/counterSlice";
 import cartReducer from "./reducers/cart/cartSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     cart: cartReducer,
   },
 });
