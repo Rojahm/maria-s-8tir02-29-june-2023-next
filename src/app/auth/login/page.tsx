bf8756f2f9d514c2f2a3e9f240e63cfa627984e2
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-08-20 23:10:48 +0330

    login page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,26 +1,44 @@
-import React from "react";
+"use client";
+import { signIn } from "next-auth/react";
+import { useRef } from "react";
 
-const LoginPage = () => {
+interface IProps {
+  searchParams?: { [key: string]: string | string[] | undefined };
+}
+
+const LoginPage = ({ searchParams }: IProps) => {
+  const userName = useRef("");
+  const pass = useRef("");
+
+  const onSubmit = async () => {
+    const result = await signIn("credentials", {
+      username: userName.current,
+      password: pass.current,
+      redirect: true,
+      callbackUrl: "/",
+    });
+  };
   return (
-    <form>
-      <label className="visually-hidden">Email address</label>
-      <input type="email" className="form-control" placeholder="Enter email" />
-      <div className="text-muted">
-        We ll never share your email with anyone else.
-      </div>
-
-      <div>
-        <label className="visually-hidden">Password</label>
+    <div>
+      {searchParams?.message && (
+        <p className="text-red-700 bg-red-100 py-2 px-5 rounded-md">
+          {searchParams?.message}
+        </p>
+      )}
+      <div className="px-7 py-4 shadow bg-white rounded-md">
         <input
-          type="password"
-          className="form-control"
-          id="inputPassword"
-          placeholder="Password"
+          placeholder="User Name"
+          type="text"
+          onChange={(e) => (userName.current = e.target.value)}
         />
+        <input
+          placeholder="Password"
+          type={"password"}
+          onChange={(e) => (pass.current = e.target.value)}
+        />
+        <button onClick={onSubmit}>Login</button>
       </div>
-
-      <button type="submit">Submit</button>
-    </form>
+    </div>
   );
 };
 
