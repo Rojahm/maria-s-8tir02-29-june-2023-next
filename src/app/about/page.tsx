913c8c98ce4ad223b25a2f2d00c8de8976c13abb
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-08-20 12:37:06 +0330

    about page content and style

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,59 @@
 const AboutPage = () => {
   return (
-    <div>
-      <h1>ABOUT</h1>
+    <div className="AboutPage">
+      <h1 className="title">ABOUT</h1>
+      <p className="content">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tincidunt
+        felis nec magna laoreet, eu convallis velit interdum. Aenean a augue at
+        odio sollicitudin imperdiet quis pretium enim. Fusce eu convallis odio,
+        in dignissim est. Duis condimentum, mi non congue dapibus, neque ipsum
+        vestibulum odio, eu commodo purus justo ut ex. Nam sed tristique lectus.
+        Nunc tempor nulla ac nisi mollis, ac dignissim mauris consequat.
+        Maecenas eu urna id turpis posuere tincidunt in eget velit. Duis ac
+        justo commodo nisl suscipit sagittis id at risus. Ut semper ipsum eget
+        pellentesque consectetur. Nam ac feugiat massa. Nam facilisis orci
+        tincidunt eros imperdiet, eu blandit sem vestibulum. Sed pulvinar in
+        magna et viverra. Ut sit amet elit ante. Pellentesque dolor neque,
+        finibus ac egestas vel, interdum eget mi. Interdum et malesuada fames ac
+        ante ipsum primis in faucibus. Fusce non porta sem. Proin est dolor,
+        consectetur vitae ipsum eget, scelerisque bibendum eros. Nam volutpat
+        vitae eros sit amet ornare. Nulla scelerisque orci et nulla imperdiet,
+        ullamcorper elementum nibh suscipit. Pellentesque sem dolor, suscipit et
+        efficitur vitae, ornare sed eros. Suspendisse condimentum sodales velit,
+        ac porta lorem luctus eu. Maecenas fermentum eros vel libero porttitor,
+        sit amet pellentesque dolor facilisis. Mauris vulputate eget libero vel
+        gravida. Nam mauris velit, tincidunt sed fermentum et, tempor vel arcu.
+        Integer eleifend bibendum nisl, eget venenatis felis lobortis sit amet.
+        Morbi finibus posuere vulputate. Donec bibendum a quam suscipit
+        lobortis. Phasellus eget aliquam eros. Ut non auctor erat. Vivamus felis
+        felis, pharetra eu erat vel, tincidunt ultricies magna. Etiam finibus
+        elementum odio, ac vulputate arcu mollis non. Sed viverra vulputate
+        venenatis. Proin auctor turpis vel lectus dictum iaculis. Pellentesque
+        sem tortor, laoreet non feugiat ac, posuere quis ante. Phasellus
+        facilisis, eros vitae efficitur porttitor, lorem lacus tincidunt massa,
+        sed viverra libero arcu convallis velit. Aliquam id turpis arcu. Class
+        aptent taciti sociosqu ad litora torquent per conubia nostra, per
+        inceptos himenaeos. Integer sed nisl nunc. Nam pulvinar consequat
+        lobortis. Pellentesque tristique vel lectus nec mollis. Praesent ornare
+        magna posuere, porta sem in, gravida massa. Curabitur pellentesque
+        hendrerit mauris, id tristique mi egestas eget. Aenean dapibus erat mi,
+        a molestie diam vulputate quis. In suscipit lacus vel nisl fermentum,
+        tincidunt volutpat metus dictum. Aliquam aliquet pretium nisl, molestie
+        laoreet nisl luctus eu. Maecenas quam turpis, dictum vitae eros quis,
+        laoreet venenatis turpis. Etiam a consequat enim. Morbi dolor eros,
+        posuere auctor dolor nec, rutrum dictum ex. Nullam faucibus at turpis
+        tincidunt rutrum. Donec mollis est nec augue pharetra laoreet. In tellus
+        sem, tincidunt ut molestie eu, aliquet eu velit. Quisque cursus commodo
+        urna, et suscipit diam efficitur ac. In vulputate leo non congue
+        consectetur. Suspendisse accumsan lobortis lacus nec pharetra. Proin
+        ultricies quis orci id sagittis. Phasellus gravida, dolor at auctor
+        commodo, diam ex sollicitudin sem, non lacinia neque turpis ac velit.
+        Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+        ridiculus mus. Integer a rhoncus tortor. Donec id nulla ornare, sagittis
+        libero id, molestie neque. Cras tristique volutpat pellentesque. Nunc
+        ipsum diam, pharetra et erat at, viverra elementum nulla. Generated 5
+        paragraphs, 503 words, 3417 bytes of Lorem Ipsum
+      </p>
     </div>
   );
 };
